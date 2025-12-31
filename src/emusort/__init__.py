873b67a5c8d5035615ddf9d41cb26ea1508e6365
smarkoco <sean_oc@emory.edu>
2025-12-30 19:30:55 -0500

    remove commented code from __init__.py

--- src/emusort/__init__.py
+++ src/emusort/__init__.py
@@ -6,17 +6,12 @@ EMUsort: A command-line tool for high-performance spike sorting of multi-channel
 
 import subprocess
 
-# import os
 
-# def version():
-#     # Retrieve version from the environment variable
-#     return os.getenv('GITHUB_REF_NAME', '0.0.0')  # Default to 'unknown' if not set
 def version():
     try:
         # Execute the git command to get the current tag
         tag = subprocess.check_output(
             ['git', 'describe', '--tags', '--abbrev=0'],
-            # cwd=repo_dir,
             stderr=subprocess.STDOUT
         ).strip().decode('utf-8')
         return tag
